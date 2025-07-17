f3c06b8428d45f287648e4f3e9f202cfdf5b1ae5
blxxdxnwvlf <deadboysolar@gmail.com>
2025-07-17 15:41:53 +0300

    Add files via upload

---
+++ E5/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double V1, V2;
+    cout << "Введите начальную скорость V1 (км/ч): ";
+    cin >> V1;
+    cout << "Введите минимальную скорость V2 (км/ч): ";
+    cin >> V2;
+
+    if (V1 <= 0 || V2 <= 0) {
+        cout << "Скорости должны быть положительными числами!" << endl;
+        return 1;
+    }
+
+    if (V2 >= V1) {
+        cout << "Минимальная скорость должна быть меньше начальной!" << endl;
+        return 1;
+    }
+
+    vector<double> speeds;
+    int lap = 1;
+    double current_speed = V1;
+
+    cout << "\nТаблица скоростей:\n";
+    cout << "Круг\tСкорость (км/ч)\n";
+    cout << "--------------------\n";
+
+    while (current_speed >= V2) {
+        speeds.push_back(current_speed);
+        cout << lap << "\t" << fixed << setprecision(2) << current_speed << endl;
+
+        current_speed *= 0.9; // Уменьшаем скорость на 10%
+        lap++;
+    }
+
+    cout << "\nТренировку следует закончить на круге " << lap - 1 << endl;
+    cout << "Скорость на последнем круге: " << speeds.back() << " км/ч" << endl;
+
+    return 0;
+}
---
+++ E6/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Kvadr(double x, double y) {
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double test_points[5][2] = {
+        {1, 1},
+        {-1, 1},
+        {-1, -1},
+        {1, -1},
+        {0, 0}
+    };
+
+    cout << "Проверка функции Kvadr(x, y):" << endl;
+    cout << "x\ty\tЧетверть" << endl;
+    cout << "---------------------" << endl;
+
+    for (int i = 0; i < 5; i++) {
+        double x = test_points[i][0];
+        double y = test_points[i][1];
+        int quadrant = Kvadr(x, y);
+
+        cout << x << "\t" << y << "\t";
+        if (quadrant == 0) {
+            cout << "На оси или в начале координат";
+        }
+        else {
+            cout << quadrant << " четверть";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
