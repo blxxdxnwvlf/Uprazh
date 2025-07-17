566697dec69dd10ce5bb0d5b0a8250380e4b7cc1
blxxdxnwvlf <deadboysolar@gmail.com>
2025-07-17 15:34:04 +0300

    Add files via upload

---
+++ E1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double N, P;
+    cout << "Введите количество листов бумаги (N): ";
+    cin >> N;
+    cout << "Введите плотность бумаги (P) в г/см²: ";
+    cin >> P;
+
+    double weight = N * P;
+
+    cout << fixed << setprecision(2);
+    cout << "Вес пачки бумаги: " << weight << " г" << endl;
+
+    return 0;
+}
