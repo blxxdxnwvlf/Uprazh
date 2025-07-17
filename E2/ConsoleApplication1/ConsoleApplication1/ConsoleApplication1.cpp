5f49ee0983758e8ebf1a59038885f525e2323588
blxxdxnwvlf <deadboysolar@gmail.com>
2025-07-17 15:38:08 +0300

    Add files via upload

---
+++ E2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    char choice;
+    do {
+        double w, h1, r, h2;
+        cout << "Введите ширину коробки (w): ";
+        cin >> w;
+        cout << "Введите высоту коробки (h1): ";
+        cin >> h1;
+        cout << "Введите радиус основания ведерка (r): ";
+        cin >> r;
+        cout << "Введите высоту ведерка (h2): ";
+        cin >> h2;
+
+        double boxVolume = w * w * h1;
+        double bucketVolume = 3.141592653589793 * r * r * h2;
+
+        cout << fixed << setprecision(2);
+        cout << "Объем коробки: " << boxVolume << endl;
+        cout << "Объем ведерка: " << bucketVolume << endl;
+
+        if (boxVolume > bucketVolume) {
+            cout << "Коробка больше по объему." << endl;
+        }
+        else if (boxVolume < bucketVolume) {
+            cout << "Ведерко больше по объему." << endl;
+        }
+        else {
+            cout << "Объемы равны." << endl;
+        }
+
+        cout << "Хотите повторить расчет? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
+} 
---
+++ E3/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+double calculateF(double x, double y) {
+    if (x > 0 && y > 0) {
+        return 1 - exp(-(x + y));
+    }
+    else if (x > 0 && y < 0) {
+        return x + y;
+    }
+    else {
+        return pow(sin(x + y), 2);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    const double step = 0.5;
+    const double y_start = 0.0;
+    const double y_end = 2.5;
+    const double x_start = 0.0;
+    const double x_end = 2.5;
+
+    cout << "y\\x |";
+    for (double x = x_start; x <= x_end + 1e-6; x += step) {
+        cout << setw(10) << fixed << setprecision(1) << x;
+    }
+    cout << endl;
+
+    cout << "----+";
+    for (double x = x_start; x <= x_end + 1e-6; x += step) {
+        cout << "----------";
+    }
+    cout << endl;
+
+    for (double y = y_start; y <= y_end + 1e-6; y += step) {
+        cout << setw(3) << fixed << setprecision(1) << y << " |";
+        for (double x = x_start; x <= x_end + 1e-6; x += step) {
+            cout << setw(10) << fixed << setprecision(4) << calculateF(x, y);
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
